01ebd3e44671a3a6c1c1620d15e7d717d7244b9f
Anelisi <anelisilanmeller@gmail.com>
2020-05-12 16:14:23 -0300

    Aprimorando o cronômetro para interação com o usuário

--- src/Tarefa_Modulo_4/Cronometro.java
+++ src/Tarefa_Modulo_4/Cronometro.java
@@ -1,8 +1,17 @@
 package Tarefa_Modulo_4;
 
+import java.util.Scanner;
+
 public class Cronometro {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int hora = 0, min = 0, seg = 0, i = 0;
+
+        System.out.println("Digite em quantos minutos o cronômetro deverá finalizar a contagem: ");
+        int c = sc.nextInt();
+
+        System.out.println("==================   INÍCIO    ================== ");
+        System.out.println("================== CRONÔMETRO  ================== ");
 
         do {
 
@@ -17,8 +26,11 @@ public class Cronometro {
                 Thread.sleep(1000);
             } catch (Exception e) {
             }
-            System.out.printf("%02d:%02d:%02d\n", hora, min, seg);
-        } while (hora < 24);
+            System.out.printf("                    %02d:%02d:%02d\n", hora, min, seg);
+        } while (min < c);
+
+        System.out.println("==================    FIM      ================== ");
+        System.out.println ("================== CRONÔMETRO  ================== ");
     }
 
 }
